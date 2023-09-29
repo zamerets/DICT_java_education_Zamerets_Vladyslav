d09d23b3d55ebbc553e6505e0a3281456ad8fe7a
Zamerets <zameretsvlad@gmail.com>
2023-09-29 13:42:47 +0300

    Test.java added to feature_branch
     added comment in Test.java at the feature branch

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class TheFirstClassJava {
+    public static void main(String[] args) {
+        System.out.println("education");
+        //comment on second string of the func//
+        System.out.println("DICT");
+    }
+}
