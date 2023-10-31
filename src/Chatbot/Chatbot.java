d356d48fed08fc87833198e8e68a9110e4108b2f
Zamerets <zameretsvlad@gmail.com>
2023-10-31 11:49:24 +0200

    Stage 1

---
+++ src/Chatbot/Chatbot.java
@@ -0,0 +1,9 @@
+package Chatbot;
+
+public class Chatbot{
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Anton");
+        System.out.println("I was created in 2023");
+
+    }
+}
