2c1dbece3ee909247700be9f42ce63f05f93c0ae
Zamerets <zameretsvlad@gmail.com>
2023-10-31 11:51:31 +0200

    Stage 2

--- src/Chatbot/Chatbot.java
+++ src/Chatbot/Chatbot.java
@@ -1,9 +1,12 @@
 package Chatbot;
-
+import java.util.Scanner;
 public class Chatbot{
     public static void main(String[] args) {
         System.out.println("Hello! My name is Anton");
         System.out.println("I was created in 2023");
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please, enter your name");
+        String name = scan.next();
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
