d7961f405be6f2b9a92c87766cc8f69a0e78763b
Zamerets <zameretsvlad@gmail.com>
2023-10-31 11:54:09 +0200

    Stage 3

--- src/Chatbot/Chatbot.java
+++ src/Chatbot/Chatbot.java
@@ -8,5 +8,12 @@ public class Chatbot{
         System.out.println("Please, enter your name");
         String name = scan.next();
         System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7");
+        int remainder3 = scan.nextInt();
+        int remainder5 = scan.nextInt();
+        int remainder7 = scan.nextInt();
+        int age =  (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 }
